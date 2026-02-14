72f5c081819c7bbf21459538cf2edc88dfcd9481
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-02-14 09:24:02 +0530

    new code

--- Day11.c
+++ Day11.c
@@ -63,3 +63,35 @@ Output:
 
 
 
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d %d",&m,&n);
+    int matrix1[m][n],matrix2[m][n],matrixSum[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+           scanf("%d", &matrix1[i][j]);
+        }
+    }
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+           scanf("%d", &matrix2[i][j]);
+        }
+    }
+    printf("OUTPUT : \n");
+    for(int i =0 ;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            matrixSum[i][j]=matrix1[i][j]+matrix2[i][j];
+            printf("%d ",matrixSum[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
