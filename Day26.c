7456464b44abe2ed6ca071e41d40b30d7c55c534
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-03-08 23:58:21 +0530

    new commit

---
+++ Day26.c
@@ -0,0 +1,21 @@
+/*Problem: Doubly Linked List Insertion and Traversal - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the linked list elements in forward order, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+
+Explanation:
+Each node has data, next, prev. Insert nodes sequentially, traverse from head using next pointer.
+*/
+
