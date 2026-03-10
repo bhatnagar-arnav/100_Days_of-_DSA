411d0c6dcfa15385bfc8517aa11c21349909dc39
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-03-10 19:22:04 +0530

    new commit

--- Day27.c
+++ Day27.c
@@ -22,3 +22,90 @@ Output:
 Explanation:
 Calculate lengths, advance pointer in longer list, traverse both simultaneously. First common node is intersection.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL) return newNode;
+    struct Node* temp = head;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+    return head;
+}
+
+int getLength(struct Node* head) {
+    int length = 0;
+    while (head != NULL) {
+        length++;
+        head = head->next;
+    }
+    return length;
+}
+
+struct Node* findIntersection(struct Node* head1, struct Node* head2) {
+    int len1 = getLength(head1);
+    int len2 = getLength(head2);
+    int diff = abs(len1 - len2);
+
+    if (len1 > len2) {
+        for (int i = 0; i < diff; i++) head1 = head1->next;
+    } else {
+        for (int i = 0; i < diff; i++) head2 = head2->next;
+    }
+
+    while (head1 != NULL && head2 != NULL) {
+        if (head1 == head2) return head1;
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    return NULL;
+}
+
+int main() {
+    int n, m, val;
+    struct Node* head1 = NULL;
+    struct Node* head2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        head1 = insertEnd(head1, val);
+    }
+
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &val);
+        head2 = insertEnd(head2, val);
+    }
+
+    if (head2 != NULL) {
+        struct Node* temp = head2;
+        while (temp->next != NULL) temp = temp->next;
+        struct Node* join = head1;
+        for (int i = 0; i < 2 && join != NULL; i++) join = join->next;
+        temp->next = join;
+    }
+
+    struct Node* intersection = findIntersection(head1, head2);
+    if (intersection != NULL) {
+        printf("%d\n", intersection->data);
+    } else {
+        printf("No Intersection\n");
+    }
+
+    return 0;
+}
---
+++ Day28.c
@@ -0,0 +1,75 @@
+/*Problem: Circular Linked List Creation and Traversal - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the circular linked list elements starting from head, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+
+Explanation:
+Last node's next points to head. Traverse from head until returning to head to avoid infinite loop.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL) {
+        head = newNode;
+        head->next = head;
+        return head;
+    }
+    struct Node* temp = head;
+    while (temp->next != head) temp = temp->next;
+    temp->next = newNode;
+    newNode->next = head;
+    return head;
+}
+
+void traverseCircular(struct Node* head) {
+    if (head == NULL) return;
+    struct Node* temp = head;
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("\n");
+}
+
+int main() {
+    int n, val;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        head = insertEnd(head, val);
+    }
+
+    traverseCircular(head);
+
+    return 0;
+}
