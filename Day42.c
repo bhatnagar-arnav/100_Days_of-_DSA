247fc78188346e3b15ddbd7ac177fba951d6c0b8
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-03-26 00:04:19 +0530

    new commit

---
+++ Day42.c
@@ -0,0 +1,18 @@
+/*Problem Statement:
+Given a queue of integers, reverse the queue using a stack.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers
+
+Output Format:
+- Print the reversed queue
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+50 40 30 20 10
+*/
